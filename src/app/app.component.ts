a63560fc05db99100ffa41d3cdc5dbad900b70bf
Airan <airan_234@hotmail.com>
2023-07-16 17:02:12 +0100

    close app with back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private platform: Platform) {}
+  ngOnInit(): void {
+    this.platform.backButton.subscribeWithPriority(0, () => {
+      App.exitApp();
+    });
+  }
 }
